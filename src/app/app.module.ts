76a861533dcd09f6b97b25cc4a088077cfc25f44
Your Name <akhilbunny98@gmail.com>
2018-07-16 14:17:26 +0530

    changes in table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule }   from '@angular/forms';
+
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { CommonModule }  from '@angular/common';
+import { HttpModule } from '@angular/http';
+
+import { TableService } from './table/table.service';
+
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
-import { FormsModule }   from '@angular/forms';
+import { TableComponent } from './table/table.component';
 
 const routes: Routes=[
   { path: 'form', component:FormComponent},
+  { path: 'table', component:TableComponent},  
   ]
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),ReactiveFormsModule,
-    FormsModule
+    FormsModule,HttpModule,HttpClientModule
 
   ],
-  providers: [],
+  providers: [TableService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/table/table.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import {TableService} from './table.service'
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent implements OnInit {
+
+  constructor(private tableService: TableService) { }
+  userData : any;
+  totalMarks: Number = 0;
+  topper :any;
+  ngOnInit() {
+    this.userSearch();
+  }
+
+  userSearch(){
+    this.tableService.userSearch().subscribe(data=>{
+      this.userData  = data;
+      this.calculateTotalMarks();
+      this.checkStatus();
+      this.setTopper();
+    })
+  };
+ 
+  calculateTotalMarks(){
+    let temp:any = [];
+    temp = this.userData;
+    let i = 0;
+    console.log(temp)
+    while(i<=temp.length-1){
+        //   for (var key in this.userData[i].marks) {
+        //     this.totalMarks += this.totalMarks + this.userData[i].marks[key]
+        //       console.log(this.totalMarks)
+        // }
+        if(temp[i].name == this.userData[i].name){
+          let totalMarks:any = 0
+          for (var key in temp[i].marks) {
+             totalMarks = totalMarks + temp[i].marks[key];
+          }
+          this.userData[i].totalMarks = totalMarks;
+        }
+      i++
+    };
+  };
+  
+  checkStatus(){
+    for(let i=0; i<=this.userData.length-1; i++){
+      let marks = this.userData[i].marks;
+          for (var key in this.userData[i].marks) {
+            if (this.userData[i].marks.hasOwnProperty(key)) {    
+                if(this.userData[i].marks[key]<20){
+                  this.userData[i].status = "Failed";
+                  break;
+                }
+                else{
+                  this.userData[i].status = "Pass";
+                }
+            }
+        }
+    };
+  }
+  setTopper(){  
+    this.topper = this.userData.reduce((min, p) => p.totalMarks > min ? p.totalMarks : min, this.userData[0].totalMarks);
+    this.userData.forEach(element => {
+      if(element.totalMarks == this.topper){
+        element.status = "Topper";
+      }
+    });
+  }
+}
